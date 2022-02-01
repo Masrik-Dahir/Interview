15151a5f6097b0d496b0914af007a5ff2ee53243
Masrik Dahir <dahirma@vcu.edu>
2022-01-31 22:24:25 -0500

    new stuff added

---
+++ Hackerrank/Diagonal_Difference.py
@@ -0,0 +1,90 @@
+# Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+#
+# For example, the square matrix  is shown below:
+#
+# 1 2 3
+# 4 5 6
+# 9 8 9
+# The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+#
+# Function description
+#
+# Complete the  function in the editor below.
+#
+# diagonalDifference takes the following parameter:
+#
+# int arr[n][m]: an array of integers
+# Return
+#
+# int: the absolute diagonal difference
+# Input Format
+#
+# The first line contains a single integer, , the number of rows and columns in the square matrix .
+# Each of the next  lines describes a row, , and consists of  space-separated integers .
+#
+# Constraints
+#
+# Output Format
+#
+# Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+#
+# Sample Input
+#
+# 3
+# 11 2 4
+# 4 5 6
+# 10 8 -12
+# Sample Output
+#
+# 15
+# Explanation
+#
+# The primary diagonal is:
+#
+# 11
+#    5
+#      -12
+# Sum across the primary diagonal: 11 + 5 - 12 = 4
+#
+# The secondary diagonal is:
+#
+#      4
+#    5
+# 10
+# Sum across the secondary diagonal: 4 + 5 + 10 = 19
+# Difference: |4 - 19| = 15
+#
+# Note: |x| is the absolute value of x
+
+
+# !/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    right_diagonal = 0
+    left_diagonal = 0
+    for row in range(0, len(arr)):
+        for col in range(0, len(arr)):
+            if col == row:
+                right_diagonal += arr[row][col]
+            if row == len(arr) - col - 1:
+                left_diagonal += arr[row][col]
+    return abs(right_diagonal - left_diagonal)
+
+    return arr
+
+
